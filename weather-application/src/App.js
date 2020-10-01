22fa2a4274be3da97b15cae58cce856ff065740e
Oleksii Marchenko <oleksiimarchenko@Oleksiis-MacBook-Pro.local>
2020-10-02 01:54:44 +0200

    Created basics

--- weather-application/src/App.js
+++ weather-application/src/App.js
@@ -1,26 +1,113 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "weather-icons/css/weather-icons.css";
+import "./App.css";
+import Weather from "./app_component/weather.component";
+import Form from "./app_component/form.component";
+const API_key = "29dce02c8a2f97ff423e9f733810cfa7";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      city: undefined,
+      country: undefined,
+      icon: undefined,
+      main: undefined,
+      celsius: undefined,
+      temp_max: undefined,
+      temp_min: undefined,
+      description: "",
+      error: false,
+    };
+    this.weatherIcon = {
+      Thunderstorm: "wi-thunderstorm",
+      Drizzle: "wi-sleet",
+      Rain: "wi-storm-showers",
+      Snow: "wi-snow",
+      Atmosphere: "wi-fog",
+      Clear: "wi-day-sunny",
+      Clouds: "wi-day-fog",
+    };
+  }
+  getWeatherIcon(icon, rangeID) {
+    switch (true) {
+      /* Thunderstorm */
+      case rangeID >= 200 && rangeID <= 232:
+        this.setState({ icon: icon.Thunderstorm });
+        break;
+      /**Drizzle */
+      case rangeID >= 300 && rangeID <= 321:
+        this.setState({ icon: icon.Drizzle });
+        break;
+      /**Rain */
+      case rangeID >= 500 && rangeID <= 531:
+        this.setState({ icon: icon.Rain });
+        break;
+      /**Snow */
+      case rangeID >= 600 && rangeID <= 622:
+        this.setState({ icon: icon.Snow });
+        break;
+      /*Atmosphere */
+      case rangeID >= 700 && rangeID <= 781:
+        this.setState({ icon: icon.Atmosphere });
+        break;
+      /*Clear*/
+      case rangeID === 800:
+        this.setState({ icon: icon.Clear });
+        break;
+      /**Clouds */
+      case rangeID >= 801 && rangeID <= 804:
+        this.setState({ icon: icon.Clouds });
+        break;
+      default:
+        this.setState({ icon: icon.Clouds });
+        break;
+    }
+  }
+  getWeather = async (e) => {
+    e.preventDefault();
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+    if (city) {
+      const api_call = await fetch(
+        `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_key}`
+      );
+      const response = await api_call.json();
+      this.setState({
+        error: false,
+        city: `${response.name} ,${response.sys.country}`,
+        celsius: this.calcCelsius(response.main.temp),
+        temp_max: this.calcCelsius(response.main.temp_max),
+        temp_min: this.calcCelsius(response.main.temp_min),
+        description: response.weather[0].description,
+      });
+      this.getWeatherIcon(this.weatherIcon, response.weather[0].id);
+    } else {
+      this.setState({ error: true });
+    }
+  };
+  calcCelsius(temp) {
+    let cell = Math.floor(temp - 273.15);
+    return cell;
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Form loadWeather={this.getWeather} error={this.state.error} />
+        <Weather
+          city={this.state.city}
+          country={this.state.country}
+          temp_celsius={this.state.celsius}
+          temp_max={this.state.temp_max}
+          temp_min={this.state.temp_min}
+          description={this.state.description}
+          weathericon={this.state.icon}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
